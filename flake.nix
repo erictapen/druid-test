e033519bc1e6d4cab825cebb63b36bc569c28b9f
Kerstin Humm <kerstin@erictapen.name>
2021-07-04 19:43:05 +0200

    first working example

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
 
-  inputs.nixpkgs.url = "github:NixOS/Nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: {
     devShell.x86_64-linux = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
